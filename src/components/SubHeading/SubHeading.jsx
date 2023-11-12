0cd51df02504e5a99522b89fa0645744edd03cd7
sgc93 <smachewgedefaw@gmail.com>
2023-11-12 00:38:09 -0900

    create reusable sub-heading component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
-);
+import React from "react";
+import img from "../../constants/images";
+const SubHeading = ({ title }) => {
+	return (
+		<div style={{ marginBottom: "1rem" }}>
+			<p className="p__cormorant">{title}</p>
+			<div className="subheading__img">
+				<img src={img.spoon} alt="spoon" className="spoon__img" />
+				<img src={img.shuka} alt="shuka" className="spoon__img" />
+			</div>
+		</div>
+	);
+};
 
 export default SubHeading;
