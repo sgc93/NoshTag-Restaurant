28a0918eb0d79059f5cd25d27372526bcf565b3e
sgc93 <smachewgedefaw@gmail.com>
2023-11-11 22:44:40 -0900

    assign image lists to variable before exporting

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,26 +1,49 @@
+import G from "../assets/G.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import findus from "../assets/findus.png";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import gericht from "../assets/gericht.png";
+import knife from "../assets/knife.png";
+import laurels from "../assets/laurels.png";
+import logo from "../assets/logo.png";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import quote from "../assets/quote.png";
+import sign from "../assets/sign.png";
+import spoon from "../assets/spoon.svg";
+import welcome from "../assets/welcome.png";
+
 const img = {
-	bg: "bg",
-	chef: "chef",
-	G: "G",
-	gallery01: "gallery01",
-	gallery02: "gallery02",
-	gallery03: "gallery03",
-	gallery04: "gallery04",
-	knife: "knife",
-	logo: "logo",
-	menu: "menu",
-	overlaybg: "overlaybg",
-	spoon: "spoon",
-	welcome: "welcome",
-	findus: "findus",
-	laurels: "laurels",
-	award01: "award01",
-	award02: "award02",
-	award03: "award03",
-	award05: "award05",
-	sign: "sign",
-	quote: "quote",
-	gericht: "gericht",
+	bg: bg,
+	chef: chef,
+	G: G,
+	gallery01: gallery01,
+	gallery02: gallery02,
+	gallery03: gallery03,
+	gallery04: gallery04,
+	knife: knife,
+	logo: logo,
+	menu: menu,
+	overlaybg: overlaybg,
+	spoon: spoon,
+	welcome: welcome,
+	findus: findus,
+	laurels: laurels,
+	award01: award01,
+	award02: award02,
+	award03: award03,
+	award05: award05,
+	sign: sign,
+	quote: quote,
+	gericht: gericht,
 };
 
 export default img;
