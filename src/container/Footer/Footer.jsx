ea52132d8e433ab05c745bb9ff4a5f791798044b
sgc93 <smachewgedefaw@gmail.com>
2023-11-13 05:26:23 -0900

    add Newsletter Component

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+	<div className="app__footer flex__center section__padding">
+		<FooterOverlay />
+		<Newsletter />
+	</div>
 );
 
 export default Footer;
