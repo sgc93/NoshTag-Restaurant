96869fc5417f1568c71a6d7a48f85b9b8b06f47d
sgc93 <smachewgedefaw@gmail.com>
2023-11-13 02:01:41 -0900

    cutout unused variables

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,3 @@
-import images from "./images";
-
 const wines = [
 	{
 		title: "Chapel Hill Shiraz",
@@ -56,29 +54,6 @@ const cocktails = [
 	},
 ];
 
-const awards = [
-	{
-		imgUrl: images.award02,
-		title: "Bib Gourmond",
-		subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-	},
-	{
-		imgUrl: images.award01,
-		title: "Rising Star",
-		subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-	},
-	{
-		imgUrl: images.award05,
-		title: "AA Hospitality",
-		subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-	},
-	{
-		imgUrl: images.award03,
-		title: "Outstanding Chef",
-		subtitle: "Lorem ipsum dolor sit amet, consectetur.",
-	},
-];
-
-const data = [wines, cocktails, awards];
+const data = [wines, cocktails];
 
 export default data;
