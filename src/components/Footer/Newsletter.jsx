58675a8abb86ad63db82fa188dd3d5f7912f1bad
sgc93 <smachewgedefaw@gmail.com>
2023-11-13 05:25:15 -0900

    create Newsletter Component for subscription

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
+	<div className="app__newsletter">
+		<div className="app__newsletter-heading">
+			<SubHeading title="Newsletter" />
+			<h1 className="headtext__cormorant">Subscribe to our Newsletter</h1>
+			<p className="p__opensans">And Never Miss Latest Updates!</p>
+		</div>
+		<div className="app__newsletter-input flex__center">
+			<input type="email" placeholder="Enter Your Email" />
+			<button className="custom__button">Subscribe</button>
+		</div>
+	</div>
 );
 
 export default Newsletter;
