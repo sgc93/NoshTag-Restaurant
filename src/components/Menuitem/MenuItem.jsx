c92abc44e651aee27703313e5ece5013bbc035a7
sgc93 <smachewgedefaw@gmail.com>
2023-11-13 00:42:43 -0900

    update SpecialMenu component | adding sample items

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./MenuItem.css";
 
-import './MenuItem.css';
-
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
-);
+const MenuItem = ({ title, tag, price }) => {
+	return (
+		<>
+			<div className="app__menuItem">
+				<div className="app__menuItem-head">
+					<div className="app__menuItem-head_name">
+						<p className="p__cormorant" style={{ color: "#DCCA87" }}>
+							{title}
+						</p>
+					</div>
+					<div className="app__menuItem-dash"></div>
+					<div className="app__menuItem-head_price">
+						<p className="p__cormorant">{price}</p>
+					</div>
+				</div>
+				<div className="app__menuItem-sub">
+					<p className="p__opensans" style={{ color: "#AAA" }}>
+						{tag}
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default MenuItem;
