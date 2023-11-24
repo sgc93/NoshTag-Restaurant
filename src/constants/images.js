b42691409335447e9f9a1f4eedd5b8108d3dabaf
sgc93 <smachewgedefaw@gmail.com>
2023-11-24 02:43:59 -0900

    update imported contents

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,26 +1,18 @@
 import N from "../assets/N.png";
-import award01 from "../assets/award01.png";
-import award02 from "../assets/award02.png";
-import award03 from "../assets/award03.png";
-import award05 from "../assets/award05.png";
 import bg from "../assets/bg.png";
 import chef from "../assets/chef.png";
-import findus from "../assets/findus.png";
 import gallery01 from "../assets/gallery01.png";
 import gallery02 from "../assets/gallery02.png";
 import gallery03 from "../assets/gallery03.png";
 import gallery04 from "../assets/gallery04.png";
 import knife from "../assets/knife.png";
-import laurels from "../assets/laurels.png";
-import logo from "../assets/logo.png";
 import menu from "../assets/menu.png";
 import noshtag from "../assets/noshtag.png";
-import overlaybg from "../assets/overlaybg.png";
 import quote from "../assets/quote.png";
 import shuka from "../assets/shuka.svg";
 import sign from "../assets/sign.png";
 import spoon from "../assets/spoon.svg";
-import welcome from "../assets/welcome.png";
+import welcome from "../assets/welcome.jpg";
 
 const img = {
 	bg: bg,
@@ -31,18 +23,10 @@ const img = {
 	gallery03: gallery03,
 	gallery04: gallery04,
 	knife: knife,
-	logo: logo,
 	menu: menu,
-	overlaybg: overlaybg,
 	spoon: spoon,
 	shuka: shuka,
 	welcome: welcome,
-	findus: findus,
-	laurels: laurels,
-	award01: award01,
-	award02: award02,
-	award03: award03,
-	award05: award05,
 	sign: sign,
 	quote: quote,
 	noshtag: noshtag,
